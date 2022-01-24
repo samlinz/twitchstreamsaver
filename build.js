018d6cca94dabcf1c7160cdbf223c8b3030dd89f
samlinz <samlinz@protonmail.com>
2022-01-24 10:16:04 +0200

    Wip

--- build.js
+++ build.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 const USERSCRIPT_CONFIG = `
 // ==UserScript==
-// @name        Twitch Stream Saver
+// @name        Twitch Stream Saver 2
 // @namespace   samlinz
 // @match       https://www.twitch.tv/*
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand
 // @icon https://static.twitchcdn.net/assets/favicon-32-d6025c14e900565d6177.png
-// @version     1.0
+// @version     $VERSION
 // @author      samlinz
 // @description 9/21/2020, 1:20:46 PM
 // ==/UserScript==
@@ -20,15 +20,36 @@ const outputDir = "./dist";
 const outputFileName = "bundle.js";
 const outputFile = path.join(outputDir, outputFileName);
 
-const onAfterBuild = () => {
+const addPrepend = ({ oldVersion }) => {
+  const newVersion = oldVersion + 1;
   const bundle = fs.readFileSync(outputFile);
-  const newBundle = [USERSCRIPT_CONFIG, bundle].join("\n");
+  const newBundle = [
+    USERSCRIPT_CONFIG.replace("$VERSION", newVersion),
+    bundle,
+  ].join("\n");
   fs.writeFileSync(outputFile, newBundle);
+};
+
+const removeTempFiles = () => {
   const files = fs.readdirSync(outputDir);
   const filesToRemove = files.filter((f) => f !== outputFileName);
   console.log("Removing", filesToRemove);
   filesToRemove.map((ftr) => path.join(outputDir, ftr)).forEach(fs.unlinkSync);
 };
+
+const onAfterBuild = ({ oldVersion }) => {
+  addPrepend({ oldVersion });
+  // removeTempFiles();
+};
+
+const oldVersion = (() => {
+  const bundle = fs.readFileSync(outputFile);
+  const match = /@version\s*(\S+)$/gim.exec(bundle);
+  const oldVersion = match.length > 1 ? Number(match[1]) : null;
+  if (!oldVersion) return 0;
+  return oldVersion;
+})();
+
 /* eslint-disable */
 require("esbuild")
   .build({
@@ -38,7 +59,7 @@ require("esbuild")
     sourcemap: false,
     outfile: outputFile,
   })
-  .then(onAfterBuild)
+  .then(onAfterBuild.bind(null, { oldVersion }))
   .then(() => {
     console.log("Done");
   })
--- dist/bundle.js
+++ dist/bundle.js
@@ -1,13 +1,13 @@
 
 // ==UserScript==
-// @name        Twitch Stream Saver
+// @name        Twitch Stream Saver 2
 // @namespace   samlinz
 // @match       https://www.twitch.tv/*
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand
 // @icon https://static.twitchcdn.net/assets/favicon-32-d6025c14e900565d6177.png
-// @version     1.0
+// @version     3
 // @author      samlinz
 // @description 9/21/2020, 1:20:46 PM
 // ==/UserScript==
